0225e434b6f6e6eff892d5d27d360b379d008e13
Mico Dioquino <mico@rush.ph>
2024-10-14 18:29:18 +0800

    404 page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -6,8 +6,8 @@ import { RouterLink } from 'vue-router';
 <template>
     <section class="text-center flex flex-col justify-center items-center h-96">
         <i class="pi pi-exclamation-triangle text-yellow-500 text-7xl mb-5"></i>
-        <h1 class="text-6xl font-bold mb-4">404 Not Found</h1>
-        <p class="text-xl mb-5">This page does not exist</p>
-        <RouterLink to="/" class="text-white bg-blue-800 hover:bg-blue-900 rounded-md px-3 py-2 mt-4">Go Back</RouterLink>
+        <h1 class="font-bold mb-2">Sorry, this page isn't available</h1>
+        <p class="text-sm text-gray-400 mb-2 w-1/2">The link you followed may be broken, or the page may have been removed.</p>
+        <RouterLink to="/" class="px-3 py-2 text-sm text-white bg-blue-800 hover:bg-blue-900 rounded-3xl mt-2">Go Back</RouterLink>
     </section>
 </template>
